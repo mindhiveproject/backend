66302fc93d4f515deb9e1cfca7b99ad4416a05b0
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-07-27 16:03:32 +0200

    google auth

--- src/resolvers/mutations/authMutations.js
+++ src/resolvers/mutations/authMutations.js
@@ -5,6 +5,7 @@ const { promisify } = require('util');
 const postmark = require('postmark');
 
 const client = new postmark.Client(process.env.MAIL_POSTMARK_CLIENT);
+const { OAuth2Client } = require('google-auth-library');
 const { transport, makeEmail } = require('../../mail');
 
 const authMutations = {
@@ -220,7 +221,7 @@ const authMutations = {
           action_url: `${process.env.FRONTEND_URL}/confirm/email?e=${args.email}&t=${confirmationToken}`,
           login_url: `${process.env.FRONTEND_URL}/login`,
           support_url: `${process.env.FRONTEND_URL}/support`,
-          product_name: 'mindHIVE',
+          product_name: 'MindHive',
           support_email: 'info@mindhive.science',
           help_url: `${process.env.FRONTEND_URL}/help/participants`,
         },
@@ -415,274 +416,206 @@ const authMutations = {
     return { message: 'OK' };
   },
 
-  // sign up a new user with token authentication identity
-  // async tokenSignUp(parent, args, ctx, info) {
-  //   args.token = args.token.toLowerCase(); // lower case token
-  //   args.username = args.username.toLowerCase(); // lower case username
-  //   if (args.email) {
-  //     args.email = args.email.toLowerCase(); // lower case username
-  //   } else {
-  //     args.email = null;
-  //   }
-  //   // TODO: if the user does not have a profile, create a profile (which will have the email auth identity)
-  //   // create a profile (which will have the token auth identity)
-  //   const profile = await ctx.db.mutation.createProfile(
-  //     {
-  //       data: {
-  //         username: args.username,
-  //         permissions: { set: ['PARTICIPANT'] },
-  //       },
-  //     },
-  //     `{ id }`
-  //   );
-  //
-  //   // create a token authentication identity
-  //   const authToken = await ctx.db.mutation.createAuthToken(
-  //     {
-  //       data: {
-  //         token: args.token,
-  //         email: args.email,
-  //         profile: {
-  //           connect: {
-  //             id: profile.id,
-  //           },
-  //         },
-  //       },
-  //     },
-  //     `{ id }`
-  //   );
-  //   console.log('created auth token identity');
-  //   // connect the token auth identity to profile
-  //   const updatedProfile = await ctx.db.mutation.updateProfile(
-  //     {
-  //       data: {
-  //         authToken: {
-  //           connect: {
-  //             id: authToken.id,
-  //           },
-  //         },
-  //       },
-  //       where: {
-  //         id: profile.id,
-  //       },
-  //     },
-  //     `{ id username permissions }`
-  //   );
-  //
-  //   // create the JWT token for user
-  //   const token = jwt.sign(
-  //     { userId: updatedProfile.id },
-  //     process.env.APP_SECRET
-  //   );
-  //
-  //   // set the jwt as a cookie on response
-  //   // const settings = checkSafari(ctx.request.headers['user-agent']);
-  //   // console.log('settings', settings);
-  //   // ctx.response.cookie('token', token, settings);
-  //   ctx.response.cookie('token', token, {
-  //     httpOnly: true,
-  //     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
-  //     sameSite: 'Strict',
-  //     secure: process.env.NODE_ENV === 'production',
-  //   });
-  //   // return user
-  //   return updatedProfile;
-  // },
-  //
-  // async tokenLogin(parent, args, ctx, info) {
-  //   args.username = args.username.toLowerCase(); // lower case token
-  //
-  //   // 2. Find the profile which has this auth identity
-  //   const profile = await ctx.db.query.profile(
-  //     {
-  //       where: {
-  //         username: args.username,
-  //       },
-  //     },
-  //     info
-  //   );
-  //   // 3. Generate the JWT token
-  //   const token = jwt.sign({ userId: profile.id }, process.env.APP_SECRET);
-  //   // 4. Set the cookie with the token
-  //   // const isSafari = checkSafari(ctx.request.headers['user-agent']);
-  //   ctx.response.cookie('token', token, {
-  //     httpOnly: true,
-  //     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
-  //     sameSite: 'Strict',
-  //     secure: process.env.NODE_ENV === 'production',
-  //   });
-  //   // 5. Return the user
-  //   return profile;
-  // },
+  async serviceSignUp(parent, args, ctx, info) {
+    // console.log('419 serviceSignUp args', args);
 
-  // send the username to the email address (if it is in the participants database)
-  // async sendParticipantUsername(parent, args, ctx, info) {
-  //   // 1. Check if it is a real participant
-  //   const email = args.email.toLowerCase(); // lower case username
-  //   const authToken = await ctx.db.query.authToken({
-  //     where: { email },
-  //   });
-  //   if (!authToken) {
-  //     throw new Error(
-  //       `There is no participant with the email address ${args.email}`
-  //     );
-  //   }
-  //
-  //   // 2. Get the username of the participant
-  //   const profiles = await ctx.db.query.profiles(
-  //     {
-  //       where: {
-  //         authToken_some: {
-  //           id: authToken.id,
-  //         },
-  //       },
-  //     },
-  //     `{id username}`
-  //   );
-  //   const usernames = profiles.map(profile => profile.username);
-  //
-  //   // 3. Email the user the usernames
-  //   const sentEmail = await client.sendEmailWithTemplate({
-  //     From: 'info@mindhive.science',
-  //     To: args.email,
-  //     TemplateAlias: 'username-reset',
-  //     TemplateModel: {
-  //       action_url: `${process.env.FRONTEND_URL}/login/token`,
-  //       support_url: `${process.env.FRONTEND_URL}/support`,
-  //       product_name: 'mindHIVE',
-  //       username: usernames[0],
-  //     },
-  //   });
-  //
-  //   return { message: 'Thanks' };
-  // },
-  //
-  // // sign up a new user with invite authentication identity
-  // async inviteSignUp(parent, args, ctx, info) {
-  //   console.log('starting invite sign up');
-  //
-  //   args.username = args.username.toLowerCase(); // lower case username
-  //
-  //   // TODO: if the user does not have a profile, create a profile (which will have the invite auth identity)
-  //   // create a profile (which will get the invite auth identity)
-  //   const profile = await ctx.db.mutation.createProfile(
-  //     {
-  //       data: {
-  //         permissions: { set: ['STUDENT'] },
-  //         username: args.username,
-  //         image: args.image,
-  //         largeImage: args.largeImage,
-  //         info: args.info,
-  //       },
-  //     },
-  //     `{ id }`
-  //   );
-  //
-  //   // create an invite authentication identity
-  //   const authInvite = await ctx.db.mutation.createAuthInvite(
-  //     {
-  //       data: {
-  //         invitedIn: {
-  //           connect: {
-  //             id: args.invitedIn,
-  //           },
-  //         },
-  //         profile: {
-  //           connect: {
-  //             id: profile.id,
-  //           },
-  //         },
-  //       },
-  //     },
-  //     `{ id }`
-  //   );
-  //   console.log('created auth invite identity');
-  //   // connect the invite auth identity to profile
-  //   // TODO connect the student to the class - make the student the member of the class
-  //   const updatedProfile = await ctx.db.mutation.updateProfile(
-  //     {
-  //       data: {
-  //         authInvite: {
-  //           connect: {
-  //             id: authInvite.id,
-  //           },
-  //         },
-  //         studentIn: {
-  //           connect: {
-  //             id: args.invitedIn,
-  //           },
-  //         },
-  //       },
-  //       where: {
-  //         id: profile.id,
-  //       },
-  //     },
-  //     `{ id username permissions }`
-  //   );
-  //
-  //   // create the JWT token for user
-  //   const token = jwt.sign(
-  //     { userId: updatedProfile.id },
-  //     process.env.APP_SECRET
-  //   );
-  //   // set the jwt as a cookie on response
-  //   // const isSafari = checkSafari(ctx.request.headers['user-agent']);
-  //   ctx.response.cookie('token', token, {
-  //     httpOnly: true,
-  //     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
-  //     sameSite: 'Strict',
-  //     secure: process.env.NODE_ENV === 'production',
-  //   });
-  //   // return user
-  //   return updatedProfile;
-  // },
-  //
-  // async inviteLogin(parent, args, ctx, info) {
-  //   args.username = args.username.toLowerCase(); // lower case username
-  //
-  //   // 1. Check whether there is a profile with that username
-  //   const profile = await ctx.db.query.profile(
-  //     {
-  //       where: {
-  //         username: args.username,
-  //       },
-  //     },
-  //     `{ id username permissions authInvite { id invitedIn {id} } }`
-  //   );
-  //
-  //   // throw error if there is no user with the provided username
-  //   if (!profile) {
-  //     throw new Error(`No such user found for username ${args.username}`);
-  //   }
-  //
-  //   // 2. Check whether the profile has an invitation and it matches the invitation that provided in login
-  //   // throw error if there is no invitations for this user exist
-  //   if (!profile.authInvite.length) {
-  //     throw new Error(`No invitations found for ${args.username}`);
-  //   }
-  //   const hosts = profile.authInvite.map(invite => invite.invitedIn.id);
-  //   console.log('hosts', hosts, args.invitedIn);
-  //
-  //   // throw error if there is the name of the host is wrong
-  //   if (!hosts.includes(args.invitedIn)) {
-  //     throw new Error(
-  //       `No invitations from the chosen host found for ${args.username}`
-  //     );
-  //   }
-  //
-  //   // 3. If there was no errors then generate the JWT token
-  //   const token = jwt.sign({ userId: profile.id }, process.env.APP_SECRET);
-  //   // 4. Set the cookie with the token
-  //   // const isSafari = checkSafari(ctx.request.headers['user-agent']);
-  //   ctx.response.cookie('token', token, {
-  //     httpOnly: true,
-  //     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
-  //     sameSite: 'Strict',
-  //     secure: process.env.NODE_ENV === 'production',
-  //   });
-  //   // Return the user
-  //   return profile;
-  // },
+    const clientID = process.env.GOOGLE_CLIENT_ID;
+    const googleClient = new OAuth2Client(clientID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: args.token,
+      audience: clientID, // Specify the CLIENT_ID of the app that accesses the backend
+    });
+    const payload = await ticket.getPayload();
+    // console.log('payload: ', payload);
+
+    args.username = payload.name.toLowerCase().trim(); // lower case username
+    if (payload.email) {
+      args.email = payload.email.toLowerCase().trim(); // lower case username
+    } else {
+      args.email = null;
+    }
+
+    // check whether the email is already in the system
+    if (args.email) {
+      const existingEmail = await ctx.db.query.authEmail(
+        {
+          where: { email: args.email },
+        },
+        `{ id }`
+      );
+      if (existingEmail) {
+        throw new Error(
+          `Email ${args.email} is taken. If you already have an account, please log in.`
+        );
+      }
+    }
+
+    // hash the password
+    const password = await bcrypt.hash(args.token, 10);
+
+    // create a profile (which will have the participant auth identity)
+    const profile = await ctx.db.mutation.createProfile(
+      {
+        data: {
+          username: args.username,
+          permissions: { set: args.permissions },
+          info: { general: args.info },
+        },
+      },
+      info
+    );
+
+    // TODO create a social account authentication identity
+    const authEmail = await ctx.db.mutation.createAuthEmail(
+      {
+        data: {
+          email: args.email,
+          password,
+          profile: {
+            connect: {
+              id: profile.id,
+            },
+          },
+          settings: {
+            googleAuth: payload,
+          },
+        },
+      },
+      `{ id }`
+    );
+    // connect the participant auth identity to profile
+    const updatedProfile = await ctx.db.mutation.updateProfile(
+      {
+        data: {
+          authEmail: {
+            connect: {
+              id: authEmail.id,
+            },
+          },
+        },
+        where: {
+          id: profile.id,
+        },
+      },
+      `{ id username permissions info}`
+    );
+
+    // join a study if there is a study to join (user, study are present in the args)
+    if (args.study && args.user) {
+      const information = {
+        ...updatedProfile.info,
+        [args.study.id]: args.user,
+      };
+      await ctx.db.mutation.updateProfile(
+        {
+          data: {
+            participantIn: {
+              connect: {
+                id: args.study.id,
+              },
+            },
+            info: information,
+          },
+          where: {
+            id: profile.id,
+          },
+        },
+        `{ id username permissions }`
+      );
+    }
+
+    // join a class if there is a class in args.class
+    if (args.class && args.class.code) {
+      await ctx.db.mutation.updateProfile(
+        {
+          data: {
+            studentIn: {
+              connect: {
+                code: args.class.code,
+              },
+            },
+          },
+          where: {
+            id: profile.id,
+          },
+        },
+        `{ id username permissions }`
+      );
+    }
+
+    // create the JWT token for user
+    const token = jwt.sign(
+      { userId: updatedProfile.id },
+      process.env.APP_SECRET
+    );
+    // set the jwt as a cookie on response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+      sameSite: 'Strict',
+      secure: process.env.NODE_ENV === 'production',
+    });
+    console.log('updatedProfile', updatedProfile);
+    // return user
+    return updatedProfile;
+  },
+
+  async serviceLogin(parent, args, ctx, info) {
+    // console.log('564 serviceLogin args', args);
+
+    const clientID = process.env.GOOGLE_CLIENT_ID;
+    const googleClient = new OAuth2Client(clientID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: args.token,
+      audience: clientID, // Specify the CLIENT_ID of the app that accesses the backend
+    });
+    const payload = await ticket.getPayload();
+    // console.log('payload: ', payload);
+
+    args.username = payload.name.toLowerCase().trim(); // lower case username
+    if (payload.email) {
+      args.email = payload.email.toLowerCase().trim(); // lower case username
+    } else {
+      args.email = null;
+    }
+
+    let profile;
+    profile = await ctx.db.query.profile(
+      {
+        where: {
+          username: args.username,
+        },
+      },
+      `{ id username permissions }`
+    );
+
+    if (!profile && args.email) {
+      const authEmail = await ctx.db.query.authEmail(
+        {
+          where: { email: args.email },
+        },
+        `{ id password profile { id username permissions } }`
+      );
+      if (!authEmail) {
+        throw new Error(`No user profile found! Please sign up first.`);
+      }
+      profile = authEmail.profile;
+    }
+
+    if (!profile) {
+      throw new Error(`No user profile found! Please sign up first.`);
+    }
+
+    const token = jwt.sign({ userId: profile.id }, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+      sameSite: 'Strict',
+      secure: process.env.NODE_ENV === 'production',
+    });
+    // return the user
+    return profile;
+  },
 };
 
 module.exports = authMutations;
