d2bd7de8d0bace90fc94d69103ae24f7181eb21c
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-09-09 15:35:05 +0200

    debug results api

--- src/resolvers/mutations/resultsMutations.js
+++ src/resolvers/mutations/resultsMutations.js
@@ -1,7 +1,7 @@
 const resultsMutations = {
   // submit a new result from open API
   async submitResultFromAPI(parent, args, ctx, info) {
-    // console.log('args', args);
+    console.log('args', args);
     const messageId = args.metadata && args.metadata.id;
     const payload = args.metadata && args.metadata.payload;
     const token = `${payload.slice(0, 4)}-${messageId}`;
