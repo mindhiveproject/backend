61eb3c238d4808a7b25542d8ffc73328a9a5301d
Yury-Shevchenko <shevchenko_yury@mail.ru>
2020-12-04 16:42:05 +0100

    display authors usernames

--- src/resolvers/queries/studiesQueries.js
+++ src/resolvers/queries/studiesQueries.js
@@ -5,23 +5,11 @@ const studiesQueries = {
     const { where } = args;
     const theclass = await ctx.db.query.class(
       { where },
-      `{ id title students { id researcherIn { id slug title createdAt participants { id } }} }`
+      `{ id title students { id researcherIn { id slug title createdAt participants { id } author { username } collaborators { username } }} }`
     );
     // 2. prepare the object to return
-    const studies = theclass.students.map(
-      student =>
-        // const studentStudies = student.researcherIn.map(studentStudy => ({
-        //   id: studentStudy.id,
-        //   title: studentStudy.title,
-        //   slug: studentStudy.slug,
-        //   participants: studentStudy.participants,
-        //   createdAt: studentStudy.createdAt,
-        // }));
-        // return studentStudies;
-        student.researcherIn
-    );
+    const studies = theclass.students.map(student => student.researcherIn);
     const uniqueStudies = [...new Set(studies.flat())];
-    console.log('studies', uniqueStudies);
     return uniqueStudies;
   },
 };
