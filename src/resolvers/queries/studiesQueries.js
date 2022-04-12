7cb7706661862f26f3e826ca12e299ba2c5d27fb
Yury-Shevchenko <shevchenko_yury@mail.ru>
2022-04-12 16:02:44 +0200

    new review board

--- src/resolvers/queries/studiesQueries.js
+++ src/resolvers/queries/studiesQueries.js
@@ -59,52 +59,37 @@ const studiesQueries = {
     return studies;
   },
 
-  // get the studies that are ready to be reviewed
-  async proposalsForReview(parent, args, ctx, info) {
-    // 1. get the classes in the class network of the user (where the user is student, mentor, or teacher)
-    const { where } = args;
-    const theClasses = await ctx.db.query.classes(
-      { where },
-      `{ id
-         title
-         students {
-           id
-          collaboratorInStudy {
-            id
-           }
-          }
-         }`
-    );
-
-    // 2. get all studies of the students (where they are collaborators)
-    const allStudentStudies = theClasses
-      .map(theClass =>
-        theClass.students.map(student =>
-          student.collaboratorInStudy.map(study => study.id)
-        )
-      )
-      .flat(3);
-
-    // 3. get other studies (featured studies and studies where other students are collaborators)
-    const otherStudies = await ctx.db.query.studies(
+  // get submitted proposals of all featured studies
+  async proposalsFeaturedStudies(parent, args, ctx, info) {
+    // 1. get featured studies
+    const featuredStudies = await ctx.db.query.studies(
       {
         where: {
-          OR: [
-            { id_in: allStudentStudies },
-            {
-              featured: true, // include featured studies (their proposals also should be submitted)
-            },
-          ],
+          featured: true,
         },
       },
       `{proposal { id }}`
     );
-    const otherStudyProposalsIDs = otherStudies
+    const ids = featuredStudies
       .map(study => study.proposal.map(proposal => proposal.id))
       .flat(2);
-    const otherProposalIDs = [...new Set([...otherStudyProposalsIDs])];
+    const proposalIds = [...new Set([...ids])];
+    // 2. pull proposals
+    const proposals = await ctx.db.query.proposalBoards(
+      {
+        where: {
+          id_in: proposalIds,
+          isSubmitted: true, // only submitted
+        },
+      },
+      info
+    );
+    return proposals;
+  },
 
-    // 4. get my studies
+  // get all proposals of my studies
+  async proposalsMyStudies(parent, args, ctx, info) {
+    // 1. get my studies
     const myStudies = await ctx.db.query.studies(
       {
         where: {
@@ -124,22 +109,53 @@ const studiesQueries = {
       },
       `{proposal { id }}`
     );
-    const myStudyProposalsIDs = myStudies
+    const ids = myStudies
       .map(study => study.proposal.map(proposal => proposal.id))
       .flat(2);
-    const myProposalIDs = [...new Set([...myStudyProposalsIDs])];
-
-    // 5. pull all proposals
+    const proposalIds = [...new Set([...ids])];
+    // 2. pull proposals
     const proposals = await ctx.db.query.proposalBoards(
       {
         where: {
-          OR: [
-            { id_in: myProposalIDs }, // submitted and not submitted
-            { id_in: otherProposalIDs, isSubmitted: true }, // only submitted
-            { author: { id: ctx.request.userId } }, // find also all proposals where the user is the author or collaborator
-            { collaborators_some: { id: ctx.request.userId } },
-          ],
+          id_in: proposalIds,
         },
+      },
+      info
+    );
+    return proposals;
+  },
+
+  // get submitted proposals of specific class
+  async proposalsOfClass(parent, args, ctx, info) {
+    // 1. get the classes
+    const { where } = args;
+    const theClasses = await ctx.db.query.classes(
+      { where },
+      `{ id
+         studies {
+           id
+           proposal {
+             id
+           }
+         }
+        }`
+    );
+    // 2. get the proposal Ids
+    const proposalIds = theClasses
+      .map(theClass =>
+        theClass.studies.map(study =>
+          study.proposal.map(proposal => proposal.id)
+        )
+      )
+      .flat(2);
+
+    // 3. Leave only unique Ids
+    const uniqueProposalIds = [...new Set([...proposalIds])];
+
+    // 4. Pull all submitted proposals
+    const proposals = await ctx.db.query.proposalBoards(
+      {
+        where: { id_in: uniqueProposalIds, isSubmitted: true }, // only submitted,
       },
       info
     );
