1ff71dc8810cd6115d011022749a6d6463d2b902
Yury-Shevchenko <shevchenko_yury@mail.ru>
2023-02-16 13:57:58 +0100

    assignment for many classes

--- src/resolvers/mutations/assignmentMutations.js
+++ src/resolvers/mutations/assignmentMutations.js
@@ -2,7 +2,7 @@ const assignmentMutations = {
   async createAssignment(parent, args, ctx, info) {
     // Check login
     if (!ctx.request.userId) {
-      throw new Error('You must be logged in to do that!');
+      throw new Error("You must be logged in to do that!");
     }
 
     const newAssignment = { ...args };
@@ -16,13 +16,12 @@ const assignmentMutations = {
               id: ctx.request.userId,
             },
           },
-          classes: args.classId
-            ? {
-                connect: {
-                  id: args.classId,
-                },
-              }
-            : null,
+          classes:
+            args.classId && args.classId.length
+              ? {
+                  connect: args.classId.map((id) => ({ id })),
+                }
+              : null,
           ...newAssignment,
         },
       },
@@ -33,15 +32,49 @@ const assignmentMutations = {
   },
 
   // update the assignment
-  updateAssignment(parent, args, ctx, info) {
+  async updateAssignment(parent, args, ctx, info) {
+    const assignment = await ctx.db.query.assignment(
+      {
+        where: { id: args.id },
+      },
+      `{ id classes { id } }`
+    );
+
+    // disconnect the current classes if needed
+    if (args.classId && args.classId.length !== assignment.classes.length) {
+      await ctx.db.mutation.updateAssignment(
+        {
+          data: {
+            classes: {
+              disconnect: assignment.classes,
+            },
+          },
+          where: {
+            id: args.id,
+          },
+        },
+        `{ id }`
+      );
+    }
+
     // take a copy of updates
     const updates = { ...args };
     // remove the ID from the updates
     delete updates.id;
+    delete updates.classId;
+
     // run the update method
     return ctx.db.mutation.updateAssignment(
       {
-        data: updates,
+        data: {
+          ...updates,
+          classes:
+            args.classId && args.classId.length
+              ? {
+                  connect: args.classId.map((id) => ({ id })),
+                }
+              : null,
+        },
         where: {
           id: args.id,
         },
@@ -60,8 +93,8 @@ const assignmentMutations = {
     );
     // check whether user has permissions to delete the item
     const ownsAssignment = assignment.author.id === ctx.request.userId;
-    const hasPermissions = ctx.request.user.permissions.some(permission =>
-      ['ADMIN'].includes(permission)
+    const hasPermissions = ctx.request.user.permissions.some((permission) =>
+      ["ADMIN"].includes(permission)
     );
     if (!ownsAssignment && !hasPermissions) {
       throw new Error(`You don't have permission to do that!`);
